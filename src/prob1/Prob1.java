19342caeea0e7452aa7eeed9d2798e4e9c3b2add
bit-user <bit-user@bit>
2017-04-26 21:11:46 +0900

    test

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -7,7 +7,8 @@ public class Prob1 {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 		
 		Scanner scanner = new Scanner( System.in  );
-		
+		//
+		//
 
 		scanner.close();
  	}
